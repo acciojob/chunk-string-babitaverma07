df5a27dc58983a2e38a09aa2eea2ebc9c9394b60
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 22:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function stringChop(str, size) {
-  // your code here
-}
+function chunkString(str, chunkLength) {
+  if (!str) {
+    return [];
+  }
 
-// Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+  const chunks = [];
+  for (let i = 0; i < str.length; i += chunkLength) {
+    chunks.push(str.substring(i, i + chunkLength));
+  }
+
+  return chunks;
+}
